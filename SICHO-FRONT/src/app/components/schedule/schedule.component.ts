c6171fbc9d3a1b376c404274bebe2158ae799fcc
Jesus Armando Valencia Chan <jearvalchan@gmail.com>
2020-04-29 15:41:19 -0600

    Horario responsive
    Las horas de clase se asignan en base a archivo remoto

--- SICHO-FRONT/src/app/components/schedule/schedule.component.ts
+++ SICHO-FRONT/src/app/components/schedule/schedule.component.ts
@@ -9,6 +9,7 @@ import {Schedule, View, EventSettingsModel, WorkWeekService} from '@syncfusion/e
 })
 export class ScheduleComponent implements OnInit {
 
+  public eventSettings:EventSettingsModel
   public profesor:any = [];
   horasE=true;
   
@@ -23,47 +24,25 @@ export class ScheduleComponent implements OnInit {
 
       let dataSource = []
       
-
       //Recorre las materias a programar y las agrega al arreglo
       this.profesor.scheduleToProgram.forEach(stp => {
         dataSource.push({
           Id: 1,
           Subject: stp.subjectName,
-          Location: 'USA',
-          StartTime: new Date(2020, 3, 27, 9, 30),
-          EndTime: new Date(2020, 3, 27, 11, 0),
+          Location: stp.classroom,
+          StartTime: new Date(2020, 3, 27, stp.start[0], stp.start[1]),
+          EndTime: new Date(2020, 3, 27, stp.end[0], stp.end[1]),
           isReadOnly:true,
+          RecurrenceRule: "FREQ=WEEKLY;BYDAY="+stp.days.toString()+";INTERVAL=1;"
         })
       });
       // Inicializa eventSettings con el arreglo de materias
       this.eventSettings = {
-        dataSource: dataSource   
-      }; 
+        dataSource:dataSource   
+      };
     })
   }
 
-  public eventSettings: EventSettingsModel = {
-    dataSource: [
-      {
-        Id: 1,
-        Subject: "Ayuda", /*profesor.scheduleToProgram.subjectName*/
-        Location: 'USA',
-        StartTime: new Date(2020, 3, 27, 9, 30),
-        EndTime: new Date(2020, 3, 27, 11, 0),
-        isReadOnly:true,
-      },
-      {
-        Id: 2,
-        Subject: "Tu sabia, lo que habia beibe", /*profesor.scheduleToProgram.subjectName*/
-        Location: 'Ella es una bandolera',
-        StartTime: new Date(2020, 3, 27, 9, 30),
-        EndTime: new Date(2020, 3, 27, 11, 0),
-        isReadOnly:true,
-      }
-
-    ]   
-  };  
-  
 imprimirHorario(){
    
    window.print();
@@ -77,64 +56,4 @@ imprimirHorario(){
    }
    
  }
-
-   
-
- /*
- export class RemoteDataComponent {
-    public selectedDate: Date = new Date(2017, 5, 5);
-    public currentView: View = 'Week';
-    public readonly: boolean = true;
-    private dataManger: DataManager = new DataManager({
-        url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/LoadData',
-        adaptor: new WebApiAdaptor,
-        crossDomain: true
-    });
- */
-  /*
-  public setView: View = 'Week'
-  public workHours: WorkHoursModel = { start: '8:00', end: '16:00' };
-  public eventObject:EventSettingsModel = {
-    dataSource: [{
-      EventTitle: "",
-      EventStart: new Date(2020, 3, 27, 9, 0),
-      EventEnd: new Date(2020, 3, 27, 10, 0),
-      isReadOnly: true,
-    },{
-      Subject: "Introducción al internet de las cosas",
-      StartTime: new Date(2020, 3, 28, 8, 30),
-      EndTime: new Date(2020, 3, 28, 10, 0),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=TU,TH;INTERVAL=1;"
-    },{
-      Subject: "Métodos ágiles de desarrollo",
-      StartTime: new Date(2020, 3, 28, 11, 30),
-      EndTime: new Date(2020, 3, 28, 13, 0),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=TU,TH;INTERVAL=1;"
-    },{
-      Subject: "Proyecto de campo I",
-      StartTime: new Date(2020, 3, 27, 13, 0),
-      EndTime: new Date(2020, 3, 27, 16, 0),
-      isReadOnly: true,
-    },{
-      Subject: "Tópico I",
-      StartTime: new Date(2020, 3, 29, 13, 0),
-      EndTime: new Date(2020, 3, 29, 14, 30),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=WE,FR;INTERVAL=1;"
-    },{
-      Subject: "Arquitectura de Software",
-      StartTime: new Date(2020, 3, 28, 15, 0),
-      EndTime: new Date(2020, 3, 28, 16, 30),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=TU,TH;INTERVAL=1;"
-    }],
-    fields: {
-      subject: {name: 'EventTitle', default: 'Tutorias'},
-      startTime: {name: 'EventStart'},
-      endTime: {name: 'EventEnd'}
-    }
-  }
-  */
 }
