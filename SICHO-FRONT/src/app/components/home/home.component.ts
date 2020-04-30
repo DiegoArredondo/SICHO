389349b834b49d42bcd6e8df38359647917bfd56
MisaMG <misaelmendozagtz@gmail.com>
2020-04-29 20:21:57 -0700

    Formulas
    Se agregan formulas para el calculo de horas en front

--- SICHO-FRONT/src/app/components/home/home.component.ts
+++ SICHO-FRONT/src/app/components/home/home.component.ts
@@ -15,11 +15,21 @@ export class HomeComponent implements OnInit {
 
   horasFaltantes= 10;
 
+  contratacionSeleccionada: string = '0';
+  distribucionSelecionada: string = '0';
+  seleccionContratacion: string='';
+  seleccionDistribucion: string='';
+  hrsInvestigacionMax: number = 0;
+  hrsClasenMax: number;
+  hrsAsesoriaMax: number;
+  hrsGestionAcademicaMax: number;
+
+
   tipoContratacion: Array<{ text:string, value: number}> = [
     {text: "Profesor Investigador Titular", value: 1},
     {text: "Profesor Investigador Auxiliar", value: 2},
     {text: "Profesor Interino", value: 3},
-    {text: "Asistente Académico Administrativo", value: 4},
+    {text: "Asistente Académico Administrativo con carga", value: 4},
   ]
 
   tipoDistribucion: Array<{ text:string, value: number}> = [
@@ -40,6 +50,90 @@ export class HomeComponent implements OnInit {
 
   }
   ngOnInit(): void {
+  }
+
+  capturarContratacion() {
+    // Pasamos el valor seleccionado a la variable seleccion1
+    this.seleccionContratacion = this.contratacionSeleccionada;
+  }
+
+  capturarDistribucion() {
+  // Pasamos el valor seleccionado a la variable seleccion1
+  this.seleccionDistribucion = this.distribucionSelecionada;
+}
+
+  determinarHorasContratacion(){
+    //calcula las horas mazimas dependiendo el tipo de contratacion
+    switch(this.contratacionSeleccionada){
+      case '1':
+        this.hrsInvestigacionMax = 20;
+        break;
+      case '2':
+        this.hrsInvestigacionMax = 20;
+        break;
+      case '3':
+        this.hrsInvestigacionMax = 6;
+        break;
+      case '4':
+        this.hrsInvestigacionMax = 6;
+        break;
+    }
+  }
+
+  determinarHorasDistribucion(){
+    //calcula las horas maximas dependiendo el tipo de distribbucion
+    switch(this.distribucionSelecionada){
+      case '1':
+        this.hrsClasenMax = 15;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '2':
+        this.hrsClasenMax = 12;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '3':
+        this.hrsClasenMax = 12;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '4':
+        this.hrsClasenMax = 9;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '5':
+        this.hrsClasenMax = 6;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '6':
+        this.hrsClasenMax = 6;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '7':
+        this.hrsClasenMax = 6;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '8':
+        this.hrsClasenMax = 6;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '9':
+        this.hrsClasenMax = 6;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+      case '10':
+        this.hrsClasenMax = 6;
+        this.hrsAsesoriaMax = Math.round(this.hrsClasenMax/3);
+        this.hrsGestionAcademicaMax = Math.round(this.hrsClasenMax/3);
+        break;
+    }
   }
 
 }
