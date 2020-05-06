65aca61852be025017e7fce87b7b8f836e2a77b8
MisaMG <misaelmendozagtz@gmail.com>
2020-05-06 13:31:55 -0700

    Desastre

--- SICHO-FRONT/src/app/components/schedule/schedule.component.ts
+++ SICHO-FRONT/src/app/components/schedule/schedule.component.ts
@@ -10,6 +10,8 @@ import {Schedule, View, EventSettingsModel, WorkWeekService} from '@syncfusion/e
 export class ScheduleComponent implements OnInit {
 
   public profesor:any = [];
+  horasE=true;
+
 
   constructor(private ProfesorService:ProfesorService) {
 
@@ -29,52 +31,14 @@ export class ScheduleComponent implements OnInit {
         EndTime: new Date(2020, 3, 27, 11, 0),
         isReadOnly:true,
       }
-    ]   
-  };  
+    ]
+  };
 
-  /*
-  public setView: View = 'Week'
-  public workHours: WorkHoursModel = { start: '8:00', end: '16:00' };
-  public eventObject:EventSettingsModel = {
-    dataSource: [{
-      EventTitle: "",
-      EventStart: new Date(2020, 3, 27, 9, 0),
-      EventEnd: new Date(2020, 3, 27, 10, 0),
-      isReadOnly: true,
-    },{
-      Subject: "Introducción al internet de las cosas",
-      StartTime: new Date(2020, 3, 28, 8, 30),
-      EndTime: new Date(2020, 3, 28, 10, 0),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=TU,TH;INTERVAL=1;"
-    },{
-      Subject: "Métodos ágiles de desarrollo",
-      StartTime: new Date(2020, 3, 28, 11, 30),
-      EndTime: new Date(2020, 3, 28, 13, 0),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=TU,TH;INTERVAL=1;"
-    },{
-      Subject: "Proyecto de campo I",
-      StartTime: new Date(2020, 3, 27, 13, 0),
-      EndTime: new Date(2020, 3, 27, 16, 0),
-      isReadOnly: true,
-    },{
-      Subject: "Tópico I",
-      StartTime: new Date(2020, 3, 29, 13, 0),
-      EndTime: new Date(2020, 3, 29, 14, 30),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=WE,FR;INTERVAL=1;"
-    },{
-      Subject: "Arquitectura de Software",
-      StartTime: new Date(2020, 3, 28, 15, 0),
-      EndTime: new Date(2020, 3, 28, 16, 30),
-      isReadOnly: true,
-      RecurrenceRule: "FREQ=WEEKLY;BYDAY=TU,TH;INTERVAL=1;"
-    }],
-    fields: {
-      subject: {name: 'EventTitle', default: 'Tutorias'},
-      startTime: {name: 'EventStart'},
-      endTime: {name: 'EventEnd'}
+  alertaHoras(){
+    if(this.horasE=true){
+      alert('Esta programando horas extra')
+    }else{
+      alert('Esta programando horas base')
     }
   }
   */
