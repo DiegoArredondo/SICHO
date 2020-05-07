bad88239b0e4e6f08f169720aaa2e964f604c199
MisaMG <misaelmendozagtz@gmail.com>
2020-05-06 19:43:47 -0700

    Formulas v2
    Vuelvo a subir las formulas en el front, que realiza el calculo de las horas maximas dependiendo las selecciones de contratacion y distribucion

--- SICHO-FRONT/src/app/components/home/home.component.ts
+++ SICHO-FRONT/src/app/components/home/home.component.ts
@@ -17,16 +17,34 @@ export class HomeComponent implements OnInit {
 
   horasFaltantes= 10;
 
-  tipoContratacion: string[] = [
-    'Profesor Investigador Titular', 'Profesor Investigador Auxiliar', 'Profesor Interino',
-    'Asistente Academico Administrativo'
-  ];
+  contratacionSeleccionada: string = '0';
+  distribucionSeleccionada: string = '0';
+  seleccionContratacion: string = '';
+  seleccionDistribucion: string = '';
+  hrsInvestigacionMax: number = 0;
+  hrsClaseMax: number;
+  hrsAsesorianMax: number;
+  hrsGestionAcademicaMax: number;
 
 
-  tipoDistribucion: string[] = [
-    'Tipo A1', 'Tipo B2', 'Tipo C', 
-    'Tipo D1', 'Tipo D2', 'Tipo D3',
-    'Tipo D4', 'Tipo D5', 'Tipo D6'
+  tipoContratacion: Array<{text:string, value: number}> = [
+    {text:"Profesor Investigador Titular", value: 1},
+    {text:"Profesor Investigador Auxiliar", value: 2},
+    {text:"Profesor Interino", value: 3},
+    {text:"Asistente Academico Administrativo", value: 4},
+  ]
+
+  tipoDistribucion: Array<{text:string, value: number}> = [
+    {text: "Tipo A", value: 1},
+    {text: "Tipo B1", value: 2},
+    {text: "Tipo B2", value: 3},
+    {text: "Tipo C", value: 4},
+    {text: "Tipo D1", value: 5},
+    {text: "Tipo D2", value: 6},
+    {text: "Tipo D3", value: 7},
+    {text: "Tipo D4", value: 8},
+    {text: "Tipo D5", value: 9},
+    {text: "Tipo D6", value: 10}
   ]
   showDropDown: boolean;
 
@@ -36,11 +54,91 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  submitSchedule(){
-   
+  capturarContratacion(){
+    //Pasamos el valor seleccionado a la variable seleccionada
+    this.seleccionContratacion = this.contratacionSeleccionada;
+  }
 
+  capturarDistribucion(){
+    this.seleccionDistribucion = this.distribucionSeleccionada;
+  }
+
+  determinarHorasContratacion(){
+    //calcula las horas maximas dependiendo el tipo de contratacion
+    switch(this.contratacionSeleccionada){
+      case '1':
+        this.hrsInvestigacionMax = 20;
+        break;
+      case '2':
+        this.hrsInvestigacionMax = 20;
+        break;
+      case '3':
+        this.hrsInvestigacionMax = 6;
+        break;
+      case '4':
+        this.hrsInvestigacionMax = 6;
+        break;
+    }
+  }
+
+    determinarHorasDistribucion(){
+      //calcula las horas maximas dependiendo el tipo de distribucion
+      switch(this.distribucionSeleccionada){
+        case '1':
+          this.hrsClaseMax = 15;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '2':
+          this.hrsClaseMax = 12;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '3':
+          this.hrsClaseMax = 12;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '4':
+          this.hrsClaseMax = 9;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '5':
+          this.hrsClaseMax = 6;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '6':
+          this.hrsClaseMax = 6;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '7':
+          this.hrsClaseMax = 6;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '8':
+          this.hrsClaseMax = 6;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '9':
+          this.hrsClaseMax = 6;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+        case '10':
+          this.hrsClaseMax = 6;
+          this.hrsAsesorianMax = Math.round(this.hrsClaseMax/3);
+          this.hrsGestionAcademicaMax = Math.round(this.hrsClaseMax/3);
+          break;
+      }
+    }
+
+  submitSchedule(){
     // Obtener el usuario en la BD de ITSON
     this.router.navigate(["schedule"])
   }
-
 }
